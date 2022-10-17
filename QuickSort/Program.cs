9a1572b69b72dacfa4239ff594b2c125e1a867b6
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-10-17 21:03:53 +0700

    Perbaikan codingan

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -26,7 +26,7 @@ namespace QuickSort
                 if (n <= 20)
                     break;
                 else
-                    Console.WriteLine("\nArray can have maximum 30 elements \n");
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
             }
             Console.WriteLine("\n====================");
             Console.WriteLine(" Enter Array Elements");
@@ -35,7 +35,7 @@ namespace QuickSort
             // get array elements
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("<" + (i + 1) + ">");
+                Console.Write("<" + (i + 1) + ">");
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
             }
@@ -55,9 +55,10 @@ namespace QuickSort
             int pivot, i, j;
             if (low > high)
                 return;
-            //partition the list into two parts :
-            //one containing element less that or equal to pivot
-            //oither containing elemets greather than pivot
+
+            //Partition the list into two parts
+            //One containing elements less that or equal to pivot
+            //Outher conntainning elements greather than pivot
 
             i = low + 1;
             j = high;
@@ -66,74 +67,73 @@ namespace QuickSort
 
             while (i <= j)
             {
-                //Search for an elements greather than pivot
-                while ((arr[i] <= pivot) && (j <= high))
+                //search for an element gretate than pivot
+                while ((arr[i] <= pivot) && (i <= high))
                 {
                     i++;
                     cmp_count++;
                 }
                 cmp_count++;
 
-                //Search for an elements less than or equal pivot
-                while ((arr[j] <= pivot) && (i <= low))
+                //Search for an element less than or equal to pivot
+                while ((arr[j] > pivot) && (j >= low))
                 {
                     j--;
                     cmp_count++;
                 }
-                cmp_count++;
 
-                if (i < j) //if the greater elements is on the left of the element
+                if (i < j) //if the greater element is on the left of the element
                 {
-                    //swap the element at index i with the element at index
+                    //swap the element at index i with the element at index j
                     swap(i, j);
                     mov_count++;
                 }
             }
-            // j now contains the index of the last element in the sorted list
+            //j now contains the index of the last element in the sorted list
 
             if (low < j)
             {
-                // move the pivot to its correct position in the list
+                //move the pivot to it's correct position in the list
                 swap(low, j);
                 mov_count++;
             }
-            //sort the list on the left of pivot using quick sort
+            //sort the list on the left of the pivot using quick sort
             q_sort(low, j - 1);
 
-            //sort the list on the right of pivot using quick sort
+            //sort the list on the right of the pivot using quick sort
             q_sort(j + 1, high);
         }
 
         void display()
         {
-            Console.WriteLine("\n---------------------");
+            Console.WriteLine("\n------------------");
             Console.WriteLine(" Sorted Array Elements ");
-            Console.WriteLine("-----------------------");
+            Console.WriteLine("--------------------");
 
             for (int j = 0; j < n; j++)
             {
                 Console.WriteLine(arr[j]);
             }
-            Console.WriteLine("\nNumber of comparisons: " + cmp_count);
+            Console.WriteLine("\nNumber of comparisons " + cmp_count);
             Console.WriteLine("\nNumber of data movements: " + mov_count);
         }
-        int getSize()
+        int getsize()
         {
             return n;
         }
         static void Main(string[] args)
         {
             //declaring the object of the class
-            Program myList = new Program();
+            Program mylist = new Program();
             //Accept array elements
-            myList.read();
-            //Calling the sorting function
-            //first call to quick sort algorithm
-            myList.q_sort(0, myList.getSize() - 1);
-            //display sorted array
-            myList.display();
+            mylist.read();
+            //calling the sorting function
+            //frist call to quick sort alogarithm
+            mylist.q_sort(0, mylist.getsize() - 1);
+            //Display sorted array
+            mylist.display();
             //to exit from the console
-            Console.WriteLine("\n\nPress Enter To Exit.");
+            Console.WriteLine("\n\nPress enter to exit. ");
             Console.Read();
         }
     }
