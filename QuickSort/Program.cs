3bb5032e6411d4ed70ed491e87e7c2c4ce9c528d
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-10-17 20:22:34 +0700

    Add method void read

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -15,5 +15,31 @@ namespace QuickSort
 
         //Number od elements in array
         private int n;
+
+        void read()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 30 elements \n");
+            }
+            Console.WriteLine("\n====================");
+            Console.WriteLine(" Enter Array Elements");
+            Console.WriteLine("\n====================");
+
+            // get array elements
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+    
+        }
     }
 }
