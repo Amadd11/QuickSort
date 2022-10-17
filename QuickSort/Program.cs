4091302bc4bb502b778dc9b7bb57700ebae5c41a
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-10-17 20:15:37 +0700

    Add Number od elements in array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -12,5 +12,8 @@ namespace QuickSort
         private int[] arr = new int[20];
         private int cmp_count = 0;
         private int mov_count = 0;
+
+        //Number od elements in array
+        private int n;
     }
 }
